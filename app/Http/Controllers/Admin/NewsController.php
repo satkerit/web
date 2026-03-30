79d0bf6e6783fcf6351e91a8e95c74d9fd6b82e8
satkerit <satkerit@gmail.com>
2026-03-30 10:30:03 +0700

    chore(news): Add logging facade import to NewsController
    - Import Illuminate\Support\Facades\Log for error tracking and debugging
    - Prepares controller for enhanced logging capabilities in news operations

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -9,6 +9,7 @@ use App\Traits\HandlesImageUpload;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
 class NewsController extends Controller
