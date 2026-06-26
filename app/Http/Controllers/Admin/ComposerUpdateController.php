f851a6eddd75c2363245a73e2874615e688de459
satkerit <satkerit@gmail.com>
2026-06-26 15:59:33 +0700

    style(admin/composer-update): remove extraneous whitespace

--- app/Http/Controllers/Admin/ComposerUpdateController.php
+++ app/Http/Controllers/Admin/ComposerUpdateController.php
@@ -144,17 +144,17 @@ class ComposerUpdateController extends Controller
             $userId = auth()->id();
             $sessionKey = 'user_last_activity_' . $userId;
             $lastActivity = \Illuminate\Support\Facades\Cache::get($sessionKey);
-            
+
             // Clear caches after update
             Artisan::call('view:clear');
             Artisan::call('route:clear');
             Artisan::call('config:clear');
-            
+
             // Clear cache but preserve user's last activity
             // Instead of clearing all cache, clear only specific cache tags or use selective clearing
             // Since we're using file cache, let's clear cache then restore user's last activity
             Artisan::call('cache:clear');
-            
+
             // Restore user's last activity cache
             if ($lastActivity) {
                 $idleTimeout = (int) config('session.idle_timeout', 15);
